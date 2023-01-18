1a29a12eca7743d1b3555a2ef944fad51bb85951
Renan Murtha <renan.murtha@gmail.com>
2023-01-18 10:59:58 -0300

    init

---
+++ js/database/alertas.js
@@ -0,0 +1,22 @@
+const max = 200;
+
+
+//SIRENE[0]
+var maxAlert = 15;
+const DATA = [
+    'A felicidade só é verdadeira quando compartilhada. -> Na Natureza Selvagem',
+    'É um mundo insano, mas nele há uma sanidade, a lealdade de velhos amigos. -> Ben-Hur',
+    'O que fazemos na vida, ecoa na eternidade. -> O Gladiador',
+    'Um homem que não passa tempo com a família não pode ser considerado um homem de verdade. -> O Poderoso Chefão',
+    'A vida passa muito rápido. Se você não parar e olhar em volta de vez em quando, você pode perdê-la. -> Curtindo a Vida Adoidado',
+    'Apenas porque uma coisa funciona, não quer dizer que ela não possa ser aprimorada. -> Pantera Negra',
+    'Esperança não é uma estratégia. -> Missão Impossível',
+    'A felicidade pode ser encontrada mesmo nas horas mais difíceis, se você se lembrar de acender a luz. -> Harry Potter e o Prisioneiro de Azkaban',
+    'Não importa quanto a vida possa ser ruim, sempre existe algo que você pode fazer e triunfar. Enquanto há vida, há esperança. -> A Teoria de Tudo',
+    'Não há lugar como o nosso lar. -> O Mágico de Oz',
+    'Compramos coisas de que não precisamos, com dinheiro que não temos, para impressionar pessoas de quem não gostamos. -> Clube da Luta',
+    'É preciso saber tirar proveito de uma desgraça, e não deixar-se vencer por ela. -> Os Intocáveis',
+    'Tudo o que temos de decidir é o que fazer com o tempo que nos é dado.O Senhor dos Anéis - A Sociedade do Anel',
+    'Você não pode viver sua vida para agradar aos outros. A escolha deve ser sua. -> Alice no País das Maravilhas',
+    'A única coisa que está entre você e seu objetivo é a história de merda que você vive contando a si mesmo sobre por que não pode alcançá-la. -> O Lobo de Wall Street'
+];
---
+++ js/functions/random-Integer.js
@@ -0,0 +1,4 @@
+//getRndInteger(99)
+function getRndInteger(max) {
+    return Math.floor(Math.random() * max);
+}
